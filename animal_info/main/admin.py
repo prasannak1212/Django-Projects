85f025a00efbab1b0181c27c61a9bed89ad85485
prasannak1212 <168375421+prasannak1212@users.noreply.github.com>
2025-02-23 01:39:13 +0530

    Added requirements.txt file for CRUD App Project

--- animal_info/main/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Animal
-
-admin.site.register(Animal)
--- animal_info/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('animal_info/<int:pk>', views.animal_info ,name='animal_info'),
-]
